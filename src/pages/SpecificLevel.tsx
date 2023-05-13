9dbd5553ef3887b44daeef417626f8d232d82bc4
mickiyas123 <mikikinfe345@gmail.com>
2023-05-13 09:29:26 +0300

    fix: add localization

--- src/pages/SpecificLevel.tsx
+++ src/pages/SpecificLevel.tsx
@@ -1,7 +1,9 @@
 import star from '@assets/icons/star.png';
 import profile from '@assets/icons/profile.png';
+import { useTranslation } from 'react-i18next';
 
 export const SpecificLevel = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex justify-center relative">
       <button className="bg-[#2E2E2E] shadow-[0px_2px_16px_rgba(255,175,82,0.8)] py-1 px-2 rounded-full h-24 w-24 flex items-center justify-center absolute  top-0">
@@ -29,7 +31,7 @@ export const SpecificLevel = () => {
             </div>
             <div className=" w-[75%] border-[5px] border-[#05C294] bg-[#008867] rounded-md p-1">
               <h1 className="text-center  text-[2rem] text-[#fff] font-bold">
-                Score
+                {t('s')}
               </h1>
               <div className="bg-[#0D0D0DBA] bg-opacity-73 rounded-md w-[90%] mx-auto">
                 <h1 className="text-center  text-[2.2rem] text-[#F8B936] font-bold">
@@ -38,10 +40,10 @@ export const SpecificLevel = () => {
               </div>
             </div>
             <button className="btn bg-[#5E5E5E] text-[1.5rem]  rounded-[12px]  text-[#FFF]  w-[75%] font-bold">
-              Level 1
+              {t('l')} 1
             </button>
             <button className="btn bg-[#F8B936] text-[1.5rem]  rounded-[12px]  text-[#FFF]  w-[75%] font-bold  border-[#FAFF00] border-4 shadow-[0px_2px_12px_rgba(250,255,0,0.5)]  mb-10">
-              Share
+              {t('sh')}
             </button>
           </div>
         </div>
