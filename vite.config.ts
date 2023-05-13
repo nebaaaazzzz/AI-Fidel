c84f396fabe2ac6f404bdf4e552d89c27506eabe
mickiyas123 <mikikinfe345@gmail.com>
2023-05-13 09:48:48 +0300

    feat: add pwa plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 import path from 'path';
 
 // https://vitejs.dev/config/
@@ -14,5 +15,15 @@ export default defineConfig({
       '@utils': path.resolve(__dirname, './src/utils')
     }
   },
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        // Manifest options...
+      },
+      workbox: {
+        // Workbox options...
+      }
+    })
+  ]
 });
