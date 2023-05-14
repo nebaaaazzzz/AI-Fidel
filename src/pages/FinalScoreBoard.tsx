fe10575cbecc7839cf584fb17e5384443fa52281
unknown <Eenrics>
2023-05-14 15:26:38 +0300

    feat: score board made respo

--- src/pages/FinalScoreBoard.tsx
+++ src/pages/FinalScoreBoard.tsx
@@ -6,19 +6,19 @@ import { useTranslation } from 'react-i18next';
 const FinalScoreBoard = () => {
   const { t } = useTranslation();
   return (
-    <LevelComplete>
-      <img src={stars} className=" mx-auto  w-2/5 mb-2" />
-      <div className=" text-lg font-semibold text-[#FFF] flex justify-center">
-        {t('fal')}
-      </div>
-      <div className="  text-5xl font-extrabold  flex justify-center">98%</div>
-      <div className=" text-lg font-semibold text-[#FFF] flex justify-center">
-        {t('iys')}
-      </div>
-      <div className="flex w-[90%] mb-4  ml-6 py-2 mt-4 bg-[#008867] font-bold justify-center border-[4px] border-[#05C294] shadow-[1px_1px_15px_rgba(0,136,103,0.5)]  rounded-xl">
-        {t('ls')}
-      </div>
-    </LevelComplete>
+        <LevelComplete>
+          <img src={stars} className=" mx-auto  md:w-2/5 w-3/5 mb-2" />
+          <div className=" text-lg font-semibold text-[#FFF] flex justify-center">
+            {t('fal')}
+          </div>
+          <div className="  text-5xl font-extrabold  flex justify-center">98%</div>
+          <div className=" text-lg font-semibold text-[#FFF] flex justify-center">
+            {t('iys')}
+          </div>
+          <div className="hidden md:flex w-[90%] mb-4  ml-6 py-2 mt-4 bg-[#008867] font-bold justify-center border-[4px] border-[#05C294] shadow-[1px_1px_15px_rgba(0,136,103,0.5)]  rounded-xl">
+            {t('ls')}
+          </div>
+        </LevelComplete>
   );
 };
 
