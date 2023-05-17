d67a3ab8155ab904402f5f3b6d9da1b663b1322f
mickiyas123 <mikikinfe345@gmail.com>
2023-05-17 08:04:39 +0300

    fix: improve service worker

--- src/swDev.js
+++ src/swDev.js
@@ -1,5 +1,10 @@
 export default function swDev() {
-  navigator.serviceWorker.register('/sw.js').then((result) => {
-    console.log('result', result);
-  });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then((result) => {
+      console.log('result', result);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
