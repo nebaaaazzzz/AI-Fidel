f71344285f53b0a21463fb4bfba1d088f5971202
mickiyas123 <mikikinfe345@gmail.com>
2023-05-17 07:57:17 +0300

    fix: improve service worker

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'my-react-app-cache-v1';
+const CACHE_NAME = 'Fidel_V1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(self.skipWaiting());
@@ -10,35 +10,55 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      // Intercept requests made through the XMLHttpRequest object
-      if (event.request.mode === 'cors' || event.request.mode === 'no-cors') {
-        return fetch(event.request).then((response) => {
+    caches.open(CACHE_NAME).then((cache) => {
+      return fetch(event.request)
+        .then((response) => {
           if (response && response.status === 200) {
-            const responseToCache = response.clone();
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, responseToCache);
-            });
+            // Update the cache with the new response
+            cache.put(event.request, response.clone());
           }
           return response;
+        })
+        .catch(() => {
+          // If the fetch fails, try to return a cached response
+          return cache.match(event.request);
         });
-      }
-
-      // Intercept requests made through the fetch API
-      return fetch(event.request).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
-          return response;
-        }
-        const responseToCache = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, responseToCache);
-        });
-        return response;
-      });
     })
   );
 });
+
+// const cacheName = 'Fidel_V1';
+
+// self.addEventListener('install', (e) => {});
+
+// // Call Activate Event
+// self.addEventListener('activate', (e) => {
+//   e.waitUntil(
+//     caches.keys().then((cacheName) => {
+//       return Promise.all(
+//         cacheName.map((cache) => {
+//           if (cache !== cacheName) {
+//             return caches.delete(cache);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+// self.addEventListener('fetch', (e) => {
+//   e.respondWith(
+//     fetch(e.request)
+//       .then((res) => {
+//         // Make copy/clone of response
+//         const resClone = res.clone();
+
+//         caches.open(cacheName).then((cache) => {
+//           // Add response to cache
+//           cache.put(e.request, resClone);
+//         });
+//         return res;
+//       })
+//       .catch((err) => caches.match(e.request).then((res) => res))
+//   );
+// });
