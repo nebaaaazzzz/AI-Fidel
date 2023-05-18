c9182f1d6dc2386afe25546c7eca93fbc581dd65
mickiyas123 <mikikinfe345@gmail.com>
2023-05-18 18:14:10 +0300

    fix: add loading bar

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -17,12 +17,12 @@ function Modal({
   return (
     <div className="absolute z-40   flex items-center justify-center px-[3%] cxm:px-[10%] md:px-0 w-[100%] h-[100%] bg-black bg-opacity-70">
       <div className="text-white shadow-2xl bg-[#000] w-[100%] h-[100%] opacity-100 flex items-center justify-center flex-col z-50 text-bold text-2xl  bg-opacity-50 border-[#F8B936] border-[3px] rounded-md">
-        <div className="flex items-center justify-center  bg-[#F8B936] rounded-md w-[45%] h-[25%]">
+        <div className="flex items-center justify-center  bg-[#F8B936] rounded-md w-[45%] h-[25%]   mt-28">
           <p className=" text-5xl font-extrabold capitalize text-[#000]">
             {nextWord}
           </p>
         </div>
-        {/* <p ref={loadingRef} className="bg-[#fff] h-2 "></p> */}
+        <p ref={loadingRef} className="bg-[#fff] h-2  mt-auto"></p>
       </div>
     </div>
   );
