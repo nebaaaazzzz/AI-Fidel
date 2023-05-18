65aa6bfdd1f9e7640e0c53d04cd2aa1b40cea5dc
mickiyas123 <mikikinfe345@gmail.com>
2023-05-18 14:58:33 +0300

    fix: make sure words are not repeated

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,9 +10,11 @@ function getRandomWordFromLevelWords(levelWords: string[]): string[] {
   const levelWordsLength = levelWords.length;
   let randomWordIndexArr: number[] = [];
   let randomWordArr: string[] = [];
-  for (let i = 0; i < 10; i++) {
+  while(randomWordIndexArr.length < 10) {
+    !randomWordIndexArr.includes(Math.floor(Math.random() * levelWordsLength)) && 
     randomWordIndexArr.push(Math.floor(Math.random() * levelWordsLength));
   }
+  // console.log(randomWordIndexArr)
   for (let i of randomWordIndexArr) {
     randomWordArr.push(levelWords[i]);
   }
@@ -24,27 +26,32 @@ export function getLevelWords(
 ): string[] {
   const words = [...passedWords];
   let levelWords: string[] = []; //to hold all words in levelIndex
-  let levelLetters: string[] =
-    []; /*contain level word from levelIndex to 0 concatinated */
+  let levelLetters: string[] = []; /*contain level word from levelIndex to 0 concatinated */
 
   for (let i = 0; i < levelIndex; i++) {
     levelLetters = levelLetters.concat(levels[i]);
   }
 
-  for (let wordIndex in words) {
-    let skip = false;
-    for (let letter of words[wordIndex]) {
-      if (!levelLetters.includes(letter)) {
-        skip = true;
+  if (levelIndex == 1) {
+    return getRandomWordFromLevelWords(words)
+  } else {
+    for (let wordIndex in words) {
+      let skip = false;
+      for (let letter of words[wordIndex]) {
+        if (!levelLetters.includes(letter)) {
+          skip = true;
+        }
+      }
+      if (!skip) {
+        levelWords.push(words[wordIndex]);
+        words.splice(Number(wordIndex), 1);
       }
     }
-    if (!skip) {
-      levelWords.push(words[wordIndex]);
-      words.splice(Number(wordIndex), 1);
-    }
+  
+    return getRandomWordFromLevelWords(levelWords);
   }
 
-  return getRandomWordFromLevelWords(levelWords);
+  
 }
 // if (!words[wordIndex].includes(letter)) {
 //   levelWords.push(words[wordIndex]);
