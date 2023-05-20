fd3da268a7730238c6ed866f6eb2a7fb5dd50523
mickiyas123 <mikikinfe345@gmail.com>
2023-05-20 09:20:25 +0300

    fix:  show different button based on score

--- src/components/LevelScore.tsx
+++ src/components/LevelScore.tsx
@@ -1,9 +1,12 @@
 import coin from '@assets/icons/coin.svg';
+import grayCoin from '@assets/icons/grayCoin.svg';
 import { useTranslation } from 'react-i18next';
 export const LevelScore = ({ level, score }) => {
   const { t } = useTranslation();
   return (
-    <div className="flex gap-4 mb-[30px]">
+    <>
+    {
+      score != '0' ? (  <div className="flex gap-4 mb-[30px]">
       <img src={coin} className=" max-w-[40px]" />
       <div className="flex w-full bg-[#008867]  py-1 pl-6 border-[4px] border-[#05C294] shadow-[1px_1px_15px_rgba(0,136,103,0.5)]  rounded-xl ">
         <span className='font-bold mr-2 mt-[2px]'>{t('l')}</span>
@@ -12,6 +15,19 @@ export const LevelScore = ({ level, score }) => {
           {score}%
         </div>
       </div>
-    </div>
+    </div>) : (
+      <div className="flex gap-4 mb-[30px]">
+      <img src={grayCoin} className=" max-w-[40px]" />
+      <div className="flex w-full bg-[#5B5B5B]  py-1 pl-6 border-[4px] border-[#A7A7A7] rounded-xl ">
+        <span className='font-bold mr-2 mt-[2px]'>{t('l')}</span>
+        <span className="text-lg font-bold">{level}</span>
+        <div className=" ml-32 text-[#F8B936]  py-0 text-lg font-bold w-1/3 rounded-md text-center bg-[#0D0D0D] bg-opacity-70">
+          {score}%
+        </div>
+      </div>
+    </div> 
+    )
+    }
+    </>
   );
 };
