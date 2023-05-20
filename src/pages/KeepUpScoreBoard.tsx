9bc35a60e9ee0e7fff34b6cd11e0afebe72a7a06
mickiyas123 <mikikinfe345@gmail.com>
2023-05-20 09:22:37 +0300

    feat:  add keep up score board

---
+++ src/pages/KeepUpScoreBoard.tsx
@@ -0,0 +1,70 @@
+import LevelComplete from './ParentScoreBoard';
+import { LevelScore } from '@/components/LevelScore';
+import { Link, useNavigate, useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+
+const useGetSearchParams = (searchParams: URLSearchParams) => {
+    const mode = searchParams.get('mode');
+    const hand = searchParams.get('hand');
+    const level = searchParams.get('level');
+    const lang = searchParams.get('lang');
+    const points = searchParams.get('points');
+    // searchParams.delete('points');
+  
+    return { mode, hand, level, lang, points };
+  };
+
+
+const LevelScoreBoard = () => {
+    let levelOne = localStorage.getItem('levelOneScore') ? localStorage.getItem('levelOneScore') : "0"
+    let levelTwo = localStorage.getItem('levelTwoScore') ? localStorage.getItem('levelTwoScore') : "0"
+    let levelThree = localStorage.getItem('levelThreeScore') ? localStorage.getItem('levelThreeScore') : "0"
+    let levelFour = localStorage.getItem('levelFourScore') ? localStorage.getItem('levelFourScore') : "0"
+    const navigate = useNavigate();
+    const searchParams = useSearchParams()[0];
+    const {
+      mode,
+      hand,
+      level,
+      lang,
+      points: score
+    } = useGetSearchParams(searchParams);
+    const factor =
+    lang == 'en' ? (mode == 'learn' ? Number(level) * 10 : 40) : 40;
+  const [results, setResult] = useState([
+    {
+      level: '1',
+      score: levelOne
+    },
+    {
+      level: '2',
+      score: levelTwo
+    },
+    {
+      level: '3',
+      score: levelThree
+    },
+    {
+      level: '4',
+      score: levelFour
+    }
+  ]);
+  return (
+    <div className="">
+        <button  onClick={() => navigate(-1)} >Back</button>
+      <LevelComplete>
+        {results.map((result, index) => {
+          return (
+            <LevelScore 
+              key={index}
+              level={result.level}
+              score={result.score}
+            />
+          );
+        })}
+      </LevelComplete>
+    </div>
+  );
+};
+
+export default LevelScoreBoard;
