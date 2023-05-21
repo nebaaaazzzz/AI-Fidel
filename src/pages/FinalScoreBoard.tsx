2f5ff38c9a54cce688b2ec9213aad6042d5399ba
mickiyas123 <mikikinfe345@gmail.com>
2023-05-21 11:18:03 +0300

    fix: remove prop and add level score for each level

--- src/pages/FinalScoreBoard.tsx
+++ src/pages/FinalScoreBoard.tsx
@@ -1,4 +1,4 @@
-import LevelComplete from './ParentScoreBoard';
+import ParentScoreBoard from './ParentScoreBoard';
 import stars from '@assets/icons/starts.png';
 import { useTranslation } from 'react-i18next';
 
@@ -6,7 +6,7 @@ import { useTranslation } from 'react-i18next';
 const FinalScoreBoard = () => {
   const { t } = useTranslation();
   return (
-        <LevelComplete>
+        <ParentScoreBoard>
           <img src={stars} className="mx-auto mt-[-15px] md:mt-0  md:w-2/5 w-[150px] mb-2" />
           <div className="text-xs mt-[15px] md:mt-0 md:text-lg font-semibold text-[#FFF] flex justify-center">
             {t('fal')}
@@ -18,7 +18,7 @@ const FinalScoreBoard = () => {
           <div className="flex ml-auto mr-auto w-full md:w-[90%] mb-4  md:ml-6 py-2 mt-4 bg-[#008867] font-bold justify-center border-[4px] border-[#05C294] shadow-[1px_1px_15px_rgba(0,136,103,0.5)]  rounded-xl">
             {t('ls')}
           </div>
-        </LevelComplete>
+        </ParentScoreBoard>
   );
 };
 
