3213738be140a961b083f6a513440797dc4f4100
mickiyas123 <mikikinfe345@gmail.com>
2023-05-23 09:14:56 +0300

    feat: add Hands Context

---
+++ src/context/HandContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState, useMemo } from "react"
+
+import { Hands } from "@mediapipe/hands";
+
+export const HandContext = createContext(null) 
+
+
+const HandContextProvider = ({ children }) => {
+  const hands = useMemo(() => {
+    let hands = new Hands ({
+      locateFile: (file) => {
+        return `/src/mediapipe/hands/${file}`;
+      }
+    });
+    hands.setOptions({
+      maxNumHands: 1,
+      modelComplexity: 1,
+      minDetectionConfidence: 0.5,
+      minTrackingConfidence: 0.5
+    });
+    return hands;
+  }, []);
+  const [hand, setHand] = useState(hands)
+  return (
+    <HandContext.Provider value={{ hand, setHand }}>
+      {children}
+    </HandContext.Provider>
+  )
+}
+
+export default HandContextProvider
