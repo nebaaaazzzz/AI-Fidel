429948ee59acca9871fcf5027733b40274b5de75
mickiyas123 <mikikinfe345@gmail.com>
2023-05-25 15:54:26 +0300

    feat: add score board

---
+++ src/pages/ScoreBoard.tsx
@@ -0,0 +1,53 @@
+import ParentScoreBoard from "./ParentScoreBoard";
+import GameKeepUpScoreBoard from "./GameKeepUpScoreBoard";
+import LearnKeepUpScoreBoard from "./LearnKeepUpScoreBoard";
+import { useState } from "react";
+
+
+  const tabsData = [
+    {
+      label: 'Learn Mode',
+      content: <LearnKeepUpScoreBoard/>
+    },
+    {
+      label: 'Game Mode',
+      content: <GameKeepUpScoreBoard/>
+    },
+  ];
+  
+  export function ScoreBoard() {
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
+  
+    return (
+      <ParentScoreBoard>
+      <div>
+        <div className="flex space-x-3 border-b w-[100%]">
+          {/* Loop through tab data and render button for each. */}
+          {tabsData.map((tab, idx) => {
+            return (
+              <button
+                key={idx}
+                className={`py-2 border-b-4  w-[50%] transition-colors duration-300 ${
+                  idx === activeTabIndex
+                    ? 'border-teal-500'
+                    : 'border-transparent hover:border-gray-200'
+                }`}
+                // Change the active tab on click.
+                onClick={() => setActiveTabIndex(idx)}
+              >
+                {tab.label}
+              </button>
+            );
+          })}
+        </div>
+        {/* Show active tab content. */}
+        <div className=" pt-4">
+          {tabsData[activeTabIndex].content}
+        </div>
+      </div>
+      </ParentScoreBoard>
+    );
+  }
+  
+
+export default ScoreBoard;
