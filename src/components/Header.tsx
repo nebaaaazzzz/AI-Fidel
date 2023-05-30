74900124c12ea640653620eb7ced38a964b4a918
mickiyas123 <mikikinfe345@gmail.com>
2023-05-30 15:37:34 +0300

    fix: remove top navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,9 @@ function Header() {
   const { t } = useTranslation();
   const links = [t('fw'), t('hp'), t('insp'), t('cmty'), t('mkt')];
   return (
-    <div className="navbar px-0 mb-10 hidden csl:flex ">
+    <div className="navbar px-0 mb-10 hidden csl:flex  ml-6">
       <LogoWithTextSM />
-      <div className="flex-1 justify-center gap-10 px-4">
+      {/* <div className="flex-1 justify-center gap-10 px-4">
         {links.map((link, index) => {
           return (
             <a href="" key={index}>
@@ -17,7 +17,7 @@ function Header() {
           );
         })}
       </div>
-      <Search />
+      <Search /> */}
     </div>
   );
 }
